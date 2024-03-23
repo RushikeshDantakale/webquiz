a935dfb72dcc3f4ad2d1e83232b24859a38a5bda
Rushi <rushikeshdantakalecoding@gmail.com>
2024-03-23 23:10:17 +0530

    added home page and made it responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Helmet } from 'react-helmet';
 
 export default function Home() {
 
-  const {pathname} = useLocation();
-  console.log(pathname , 8);
+  const { pathname } = useLocation();
+  console.log(pathname, 8);
   return (<>
-  <Helmet>
-   <title>ReactQuiz - Home</title>
-  </Helmet>
-    <Navbar sub={"home"}/>
-    {pathname}
-    </>
+    <Helmet>
+      <title>ReactQuiz - Home</title>
+    </Helmet>
+    <Navbar sub={"home"} />
+    <div className='home-container'>
+      <div className='home-left'>
+       <div className="inner">
+        <div className="title">
+          Learn <br />
+          new concepts <br />
+          for each question
+        </div>
+
+        <div className="description">
+        We help you prepare for exams and quizes !
+        </div>
+        <Link to="/user/register">
+        <button>
+          Start Solving
+        </button>
+        </Link>
+
+        </div>
+
+      </div>
+      <div className='home-right'> <img className='right-img' src="./home-right.svg" /> </div>
+    </div>
+  </>
   )
 }
