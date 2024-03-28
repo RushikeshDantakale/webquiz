fc0a3e3ae387630f06a8901ef88db883dca8ec09
Rushi <rushikeshdantakalecoding@gmail.com>
2024-03-28 12:08:14 +0530

    admin login functionality.

--- src/admin-components/QuestionsMain.jsx
+++ src/admin-components/QuestionsMain.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 export default function QuestionsMain() {
-  return (
-    <div>QuestionsMain</div>
+  return (<>
+    
+  </>
   )
 }
--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -2,8 +2,14 @@ import React, { useState } from 'react'
 import Navbar from '../../components/Navbar'
 import {useLocation, useNavigate } from 'react-router-dom'
 import { Helmet } from 'react-helmet';
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
+
+  console.log(import.meta.env.VITE_SERVER , 9);
+
 
   const [data , setData] = useState({
     email:"",
@@ -14,10 +20,30 @@ export default function Login() {
   const navigate = useNavigate();
   console.log(pathname, 8);
 
-  const submitData = (e) =>{
+  const submitData =async (e) =>{
     e.preventDefault();
-    navigate("/admin/dashboard");
-  }
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_SERVER}/admin/login`, data);
+      console.log(response, 21);
+  
+      if (response.status === 200) {
+        // Login successful
+          return toast.success(response.data.message, { position: "top-right" });
+      
+        navigate("/admin/dashboard");
+      } else if (response.status === 401) {
+        // Unauthorized
+        toast.error("Invalid Username or password", { position: "top-right" });
+      } else {
+        // Other error
+        toast.error("An error occurred. Please try again.", { position: "top-right" });
+      }
+    } catch (error) {
+      // Handle network or other errors
+      console.error("Error:", error);
+      toast.error("An error occurred. Please try again.", { position: "top-right" });
+    }
+    }
 
   const changeFormData = (e) => {
     e.preventDefault();
@@ -32,7 +58,7 @@ export default function Login() {
     </Helmet>
       <Navbar sub={"home"}/>
       <div className="flex j-center login-outer">
-      <form method='post' onSubmit={(e) => submitData(e)} action="">
+      <form method='post' onSubmit={(e) => submitData(e)} >
         <div className='rounded-block login'>
           <h1>Login Here...</h1>
           <p>*For Admin Only..*</p>
@@ -42,6 +68,7 @@ export default function Login() {
         </div>
       </form>
       </div>
+      <ToastContainer/>
     </>
   )
 }
