579bec98a6aa4b81434d7c3b0de3ac7119b758b7
Rushi <rushikeshdantakalecoding@gmail.com>
2024-04-02 09:17:53 +0530

    changed bootstrap.min.css import to index.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import './App.css'
 import Home from './pages/Home';
