55a0037dfb529ef716cdadacc157a6e63cfa2c64
Rushi <rushikeshdantakalecoding@gmail.com>
2024-04-02 09:25:36 +0530

    moved botstrap js import to main.js claude fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      'bootstrap': 'bootstrap/dist/js/bootstrap.bundle.min.js'
+    }
+  },
   plugins: [react()],
 })
