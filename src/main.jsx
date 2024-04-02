08870e79661e404d9c6875db4753599107d2a248
Rushi <rushikeshdantakalecoding@gmail.com>
2024-04-02 09:30:35 +0530

    changed filename to bootstrap.bndle.js in dist/js folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "bootstrap/dist/js/bootstrap.bundle.js"
 import App from './App.jsx'
-import "bootstrap/dist/js/bootstrap.min.js";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      'bootstrap': 'bootstrap/dist/js/bootstrap.bundle.min.js'
-    }
-  },
+  // resolve: {
+  //   alias: {
+  //     'bootstrap': 'bootstrap/dist/js/bootstrap.bundle.min.js'
+  //   }
+  // },
   plugins: [react()],
 })
